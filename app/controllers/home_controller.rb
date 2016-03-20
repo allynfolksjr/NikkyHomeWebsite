03b36c70bdbaec4e4bef6c2652b257311ee24fdc
Nikky Southerland <git@nikkysoutherland.com>
2016-03-19 22:28:05 -0700

    Add Twitter timeline support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
-require 'wordpress'
+require 'nikky/wordpress'
+require 'nikky/twitter'
 
 class HomeController < ApplicationController
   def index
     @wordpress_posts = Rails.cache.fetch('wordpress_posts', race_condition_ttl: 10, expires_in: 1.hour) do
       Nikky::Wordpress.new.recent_posts
+    end
+
+    @tweets = Rails.cache.fetch('tweets', race_condition_ttl: 10, expires_in: 1.hour) do
+      Nikky::Twitter.new.recent_tweets
     end
   end
 end
--- lib/nikky/twitter.rb
+++ lib/nikky/twitter.rb
@@ -1,35 +1,41 @@
 module Nikky
   class Twitter
     class Tweet
-      attr_accessor :text, :posted_at, :uri
-      def initialize(text, posted_at, uri)
+      attr_accessor :text, :posted_at, :url
+      def initialize(text, posted_at, url)
         @text = text
         @posted_at = posted_at
-        @uri = uri
+        @url = url
       end
     end
 
     def recent_tweets
-      client = Twitter::REST::Client.new do |config|
+      client = ::Twitter::REST::Client.new do |config|
         config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']}
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       end
 
-      # We have to fetch 100 tweets because the count includes replies and rts
-      # For instance, if user had 38 replies, 42rts, 100 would return a 20-length
+      # We have to fetch 200 tweets because the count includes replies and rts
+      # For instance, if user had 38 replies, 142rts, 200 would return a 20-length
       # array :|
-      tweets = client.user_timeline(
+      unrefined_tweets = client.user_timeline(
         'allynfolksjr',
-        count: 100,
+        count: 200,
         exclude_replies: true,
         include_rts: false
-      )
+        )
 
       # Hacky Way to not include a blog post in the twitter timeline on the site
-      tweets = tweets.delete_if{|t| t.source =~ /WordPress\.com/}
+      # Additionally, we don't want to include a subtweet, tweet with link, or
+      # tweet with images for ideological pureness
+      unrefined_tweets = unrefined_tweets.delete_if do |t|
+        t.media? ||
+        t.entities? ||
+        t.source =~ /WordPress\.com/
+      end
 
-      tweets.map do |tweet|
-        Tweet.new(tweet.full_text, tweet.created_at, tweet.uri)
+      unrefined_tweets.map do |tweet|
+        Tweet.new(tweet.full_text, tweet.created_at, tweet.uri.to_s)
       end
 
     rescue StandardError => e
