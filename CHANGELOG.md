066b38647deea31d48061ba7e2f0dfcf7b957fdd
Nikky Southerland <git@nikkysoutherland.com>
2016-03-19 21:02:48 -0700

    Fix bug wrt cache

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.1.1
+
+* Bug Fix 
+
 # 1.1.0
 
 * WordPress.com integration to show recent blog posts
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ require 'wordpress'
 
 class HomeController < ApplicationController
   def index
-    Rails.cache.fetch('wordpress_posts', race_condition_ttl: 10, expires_in: 1.hour) do
-      @wordpress_posts = Wordpress.new.recent_posts
+    @wordpress_posts = Rails.cache.fetch('wordpress_posts', race_condition_ttl: 10, expires_in: 1.hour) do
+      Wordpress.new.recent_posts
     end
   end
 end
