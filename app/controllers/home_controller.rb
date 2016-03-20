c5ccedb58910dc7a05ddce529c939c720f78ce20
Nikky Southerland <git@nikkysoutherland.com>
2016-03-19 20:56:33 -0700

    Show recent blog posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
+require 'wordpress'
+
 class HomeController < ApplicationController
   def index
+    Rails.cache.fetch('wordpress_posts', race_condition_ttl: 10, expires_in: 1.hour) do
+      @wordpress_posts = Wordpress.new.recent_posts
+    end
   end
 end
